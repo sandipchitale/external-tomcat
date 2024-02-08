ed7be506f1be2281148f83a86091cfc0a85a9af7
Sandip Chitale <sandipchitale@gmail.com>
2024-02-07 23:08:41 -0800

    Pass spring.config.location.

--- src/main/java/com/opentext/ia/externaltomcat/ExternalTomcatApplication.java
+++ src/main/java/com/opentext/ia/externaltomcat/ExternalTomcatApplication.java
@@ -5,8 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ExternalTomcatApplication extends SpringBootServletInitializer {
@@ -14,9 +20,25 @@ public class ExternalTomcatApplication extends SpringBootServletInitializer {
 	@RestController
 	public static class IndexController {
 
-		@GetMapping("/")
+		private String env;
+        public IndexController(Environment environment) {
+			Map<String, Object> map = new HashMap<>();
+			if (environment instanceof  ConfigurableEnvironment configurableEnvironment) {
+				MutablePropertySources propertySources = configurableEnvironment.getPropertySources();
+				propertySources.stream().forEach((PropertySource<?> propertySource) -> {
+					if (propertySource instanceof MapPropertySource mapPropertySource) {
+						map.putAll(mapPropertySource.getSource());
+					}
+				});
+			}
+			String string = map.toString();
+			string = string.substring(1, string.length() - 1);
+			env = Arrays.stream(string.split(", ")).sorted().collect(Collectors.joining("<br/>"));
+        }
+
+        @GetMapping("/")
 		public String index(HttpServletRequest request) {
-			return request.getServletContext().getServerInfo();
+			return env;
 		}
 	}
 
